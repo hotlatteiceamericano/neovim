050660148d92b479882bd54ce45bf4e9a3f79773
hotlatteiceamericano <hotlatteiceamericano@gmail.com>
2026-01-20 15:06:01 +0800

    using telescope for reference (preview) and recent buffers (finally!)

--- lua/bjc/plugins/telescope.lua
+++ lua/bjc/plugins/telescope.lua
@@ -51,7 +51,23 @@ return {
         defaults = {
           file_ignore_patterns = { 'target/.*' },
         },
-        -- pickers = {}
+        pickers = {
+          buffers = {
+            sort_mru = true,
+            sort_lastused = true,
+            ignore_current_buffer = false,
+            theme = 'dropdown', -- Optional: use dropdown theme
+            previewer = true,
+            mappings = {
+              i = {
+                ['<c-d>'] = 'delete_buffer',
+              },
+              n = {
+                ['<c-d>'] = 'delete_buffer',
+              },
+            },
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
@@ -75,6 +91,7 @@ return {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
